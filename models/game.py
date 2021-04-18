85b64f2a93ade0eb93bc967c11a1a968c528f338
Allen_K <allenkelly278@gmail.com>
2021-04-18 14:31:18 +0100

    add game logic

--- models/game.py
+++ models/game.py
@@ -1,4 +1,5 @@
 from models.player import Player
+from random import choice
 
 class Game():
     """ a game class with functions to play the game ROCK PAPER SCISSORS """
@@ -26,12 +27,31 @@ class Game():
 
         self._assign_players(args)
 
+        # if weapons are equal, it's a draw
+        if self.player1.weapon == self.player2.weapon:
+            return
+
+        # check all possible winning scenarios for player1
+        if self.player1.gesture == "rock" and self.player2.gesture == "scissors":
+            return self.player1
+        if self.player1.gesture == "paper" and self.player2.gesture == "rock":
+            return self.player1
+        if self.player1.gesture == "scissors" and self.player2.gesture == "paper":
+            return self.player1 
+
+        # if we make it this far, player2 must have won
+        return self.player2 
+        
+
 
     def _assign_players(self, players):
         # if no second player is present, create a computer player
         # and assign a random choice of weapon
         self.player1 = players[0]
 
-        if len(args) == 1:
+        if len(args) == 2:
+            self.player2 = players[1]
+        else:
             weapon = ["rock", "paper", "scissors"]
+            self.player2 = Player("computer", choice(gestures))
             
