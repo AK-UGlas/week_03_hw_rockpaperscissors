dc2ed18b2b1c741985cbde47dce921b89f6f8bd5
Allen_K <allenkelly278@gmail.com>
2021-04-18 20:15:13 +0100

    main functionality complete

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,12 +1,25 @@
 from flask import render_template, request
 from app import app
-from models.game import game
-from models.player import player
+from models.game import Game
+from models.player import Player
+
+# declare instance of Game class here. we can reuse any time a new game starts
+rps = Game()
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", title='Welcome')
 
 @app.route("/", methods=["POST"])
 def play_game():
-    print(request.form)
+    winner = None
+    player1 = Player(request.form["name-1"], request.form["weapon-1"])
+
+    # check both player details have been completed
+    if request.form.get('name-2'):
+        player2 = Player(request.form["name-2"], request.form["weapon-2"])
+        winner = rps.play_game(player1, player2)
+    else:
+        winner = rps.play_game(player1)
+
+    return render_template('result.html', title='Result', winner=winner, weapon=winner.weapon if winner else player1.weapon)
--- models/game.py
+++ models/game.py
@@ -7,7 +7,7 @@ class Game():
         self.player1 = None
         self.player2 = None
 
-    def play_rpc(self, *args):
+    def play_game(self, *args):
         # play a game of rock paper scissors
         # compare the weapon (gesture) of 2 players and decide the outcome based on the following rules:
         #   Rock beats Scissors
@@ -32,11 +32,11 @@ class Game():
             return
 
         # check all possible winning scenarios for player1
-        if self.player1.gesture == "rock" and self.player2.gesture == "scissors":
+        if self.player1.weapon == "rock" and self.player2.weapon == "scissors":
             return self.player1
-        if self.player1.gesture == "paper" and self.player2.gesture == "rock":
+        if self.player1.weapon == "paper" and self.player2.weapon == "rock":
             return self.player1
-        if self.player1.gesture == "scissors" and self.player2.gesture == "paper":
+        if self.player1.weapon == "scissors" and self.player2.weapon == "paper":
             return self.player1 
 
         # if we make it this far, player2 must have won
@@ -48,9 +48,9 @@ class Game():
         # and assign a random choice of weapon
         self.player1 = players[0]
 
-        if len(args) == 2:
+        if len(players) == 2:
             self.player2 = players[1]
         else:
-            weapon = ["rock", "paper", "scissors"]
-            self.player2 = Player("computer", choice(gestures))
+            weapons = ["rock", "paper", "scissors"]
+            self.player2 = Player("computer", choice(weapons))
             
